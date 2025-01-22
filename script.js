7101e977cb64a903dbe4f03c207dc4793b1f2d35
Nestor M <ntor2004@gmail.com>
2025-01-22 09:48:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback');
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+
+        const username = document.getElementById('username').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim();
+
+        let isValid = true;
+        let messages=[]; 
+        
+        if(username.length<3){
+            isValid=false;
+            messages.push('user name should be more than 3 characters');
+        }
+        if (!email.includes("@") ||(!email.includes("."))){
+            isValid = false;
+            messages.push('Email should contain "@" and "."');
+        }
+       
+        if(password.length<8){
+            isValid=false;
+            messages.push('password should be atleast 8characters');
+        }
+
+        feedbackDiv.style.display='block';
+        if (isValid){
+            feedbackDiv.textContent="Registration successful!";
+            feedbackDiv.style.color="#28a745";
+            
+        }
+        else{
+            feedbackDiv.textContent=messages.join('<br>');
+            feedbackDiv.style.color="#dc3545";
+        }
+    });
+});
+
+    
+
